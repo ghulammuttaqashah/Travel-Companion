45c5640bb134efbcdb08105ae39827501825e880
Ghulam Muttaqa Shah <ghulammuttaqashah@gmail.com>
2025-06-23 22:46:27 +0500

    updated Weather.jsx fixed a small bug

--- client/travel-companion-app/src/pages/Weather.jsx
+++ client/travel-companion-app/src/pages/Weather.jsx
@@ -1,32 +1,18 @@
-import { useState } from "react";
 import WeatherDisplay from "../components/WeatherDisplay";
 import FavoriteCitiesList from "../components/FavoriteCitiesList";
-import Spinner from "../components/Spinner";
+import { useState } from "react";
 
 function Weather() {
   const [refreshFavorites, setRefreshFavorites] = useState(false);
-  const [loading, setLoading] = useState(true);
 
   const handleFavoriteAdded = () => {
-    setRefreshFavorites((prev) => !prev);
+    setRefreshFavorites((prev) => !prev); // trigger refresh
   };
 
   return (
-    <div className="app-container flex justify-center items-center min-h-screen px-4">
-      {loading ? (
-        <Spinner />
-      ) : (
-        <div className="w-full">
-          <WeatherDisplay
-            onFavoriteAdded={handleFavoriteAdded}
-            setLoading={setLoading}
-          />
-          <FavoriteCitiesList
-            refreshTrigger={refreshFavorites}
-            setLoading={setLoading}
-          />
-        </div>
-      )}
+    <div className="app-container">
+      <WeatherDisplay onFavoriteAdded={handleFavoriteAdded} />
+      <FavoriteCitiesList refreshTrigger={refreshFavorites} />
     </div>
   );
 }
