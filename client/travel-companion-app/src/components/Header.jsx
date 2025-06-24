5f62e7396e30ffe8462914735aa5d3e1920df252
Ghulam Muttaqa Shah <ghulammuttaqashah@gmail.com>
2025-06-24 14:36:43 +0500

    added some css

--- client/travel-companion-app/src/components/Header.jsx
+++ client/travel-companion-app/src/components/Header.jsx
@@ -59,7 +59,7 @@ function Header() {
       </h1>
 
       {/* Desktop Nav */}
-      <ul className="hidden md:flex space-x-8 items-center text-xl font-semibold">
+      <ul className="hidden md:flex space-x-8 items-center text-xl cursor-pointer font-semibold">
         <li><Link to="/" className="hover:text-white transition">Home</Link></li>
         <li><button onClick={() => handleProtectedNav("/weather")} className="hover:text-white transition">Weather</button></li>
         <li><button onClick={() => handleProtectedNav("/currency-converter")} className="hover:text-white transition">Currency Converter</button></li>
@@ -101,7 +101,7 @@ function Header() {
 
       {/* Mobile Nav */}
       {isMenuOpen && (
-        <ul className="absolute top-full left-0 w-full bg-[#243642] text-[#E2F1E7] flex flex-col items-center space-y-6 py-6 md:hidden z-10 text-xl font-medium shadow-md border-t border-[#243642]">
+        <ul className="absolute top-full left-0 w-full bg-[#243642] text-[#E2F1E7] flex flex-col items-center space-y-6 py-6 md:hidden z-10 text-xl font-medium shadow-md border-t border-[#243642] cursor-pointer">
           <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
           <li><button onClick={() => handleProtectedNav("/weather")}>Weather</button></li>
           <li><button onClick={() => handleProtectedNav("/currency-converter")}>Currency Converter</button></li>
