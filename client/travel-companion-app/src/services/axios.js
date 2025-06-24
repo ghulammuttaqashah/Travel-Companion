05ee100c7eaee57c64cda815fd939d80d8530dd7
Ghulam Muttaqa Shah <ghulammuttaqashah@gmail.com>
2025-06-24 13:25:27 +0500

    fixed the axios error due to syntax

--- client/travel-companion-app/src/services/axios.js
+++ client/travel-companion-app/src/services/axios.js
@@ -1,3 +1,12 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 const PUBLIC_ROUTES = ['/', '/login', '/register', '/session-expired'];
 
 axiosInstance.interceptors.response.use(
@@ -16,3 +25,4 @@ axiosInstance.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+export default axiosInstance;
